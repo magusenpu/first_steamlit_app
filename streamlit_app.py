2c6281d754143274d7309ab23b60f45c3ca8209c
magusenpu <116260458+magusenpu@users.noreply.github.com>
2022-10-20 10:40:52 +0200

    Update streamlit_app.py
    Fill in text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
+
+streamlit.header('Breakfast menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach, & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free_Range Egg')
